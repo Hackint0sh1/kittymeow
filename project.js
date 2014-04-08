e4fa110a06936b01d90cced64a0f2bb788a8afc8
EWB User <ewb@unassigned-hostname>
2014-04-08 16:18:00 -0400

    Cherry movement enabled(Defined as cherryY)

--- project.js
+++ project.js
@@ -10,7 +10,7 @@ var exampleVar = 10;
 var x = 100;
 var y = 460;
 var xdirection = 1;
-
+var cherryY = 15;
 // put functions here, just like kahn academy
 var exampleFunction = function() {
     rect(30, 20, 80, 80)	;
@@ -19,7 +19,7 @@ var exampleFunction = function() {
 
 // this code is executed once when the program is started
 var  setup = function() {
- b = loadImage("barn1.jpg");
+    b = loadImage("barn1.jpg");
     pig = loadImage("piggy.png");
     cherry = loadImage("cherry1.png");
     gameOver = loadImage("Gameover.png");
@@ -46,10 +46,11 @@ var draw = function() {
 	xdirection = xdirection*-1;
     }
     randomx += xdirection;
-
+    cherryY += 1;
     image(b, 0, 0);
-    image(cherry, randomx, 400);
-    ;
+    image(cherry, randomx, cherryY);
+    
+    
 
     if(x > 0 && keyIsPressed && keyCode == LEFT ) {
 	x = x - 33;
