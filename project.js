f1af040a1bca221ddbebae11e5631d9813ad4240
EWB User <ewb@unassigned-hostname>
2014-06-03 17:06:57 -0400

    Readded point array counter

--- project.js
+++ project.js
@@ -11,43 +11,61 @@ var x = 100;
 var y = 460;
 var xdirection = 1;
 var cherryY = 15;
+var music;
+
 // put functions here, just like kahn academy
 var exampleFunction = function() {
     rect(30, 20, 80, 80)	;
 };
+var counter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 
 // this code is executed once when the program is started
 var  setup = function() {
+
     b = loadImage("barn1.jpg");
-    pig = loadImage("piggy.png");
+    pig = loadImage("pig.png");
     cherry = loadImage("CanOfSoup.png");
-    gameOver = loadImage("Gameover.png");
+    
     // set up the size of the canvas (you probably don't want to change this!)
     size(800, 600);
 
+    var setup = function() {
 
-    
-};
-var keyIsPressed = false;
+    };
+    var keyIsPressed = false;
 
-var  keyPressed = function() {
-    keyIsPressed = true;
+    var  keyPressed = function() {
+	keyIsPressed = true;
+    }
+
+    var keyReleased = function() {
+	keyIsPressed = false;
+    }
 }
+var draw = function() {
+	music = new Audio("SSSU.mp3");
+	music.controls = true;
+	music.loop = true;
+	music.autoplay = true;
+	document.body.appendChild(music);
 
-var keyReleased = function() {
-    keyIsPressed = false;
 }
-
 
 var draw = function() {
-
+    
     if (Math.random() < 0.01) {
 	xdirection = xdirection*-1;
     }
     randomx += xdirection;
     cherryY += 1;
     image(b, 0, 0);
+
+    textSize(43);
+    text(counter[9], 44, 53);
+    textSize(18);
+    text("POINTS", 23, 20);
+
     image(cherry, randomx, cherryY);
     
     if(cherryY > 600) {
@@ -68,16 +86,10 @@ var draw = function() {
 
     image(pig, x, y);
 
-    if (x > 700) {
-	image(gameOver, 0, 0);
-    }
-    if (cherry > x && cherry < x + 75 && cherry > y && cherry < y + 150) {
-        cherry = 15;
-    }
     
-	
-
     
-  
+    
+}
 
-};
+
+
